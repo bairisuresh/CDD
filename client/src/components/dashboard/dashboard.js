eeb30f0e7d6c702da412137dcee489f69e855a12
suresh bairi <suresh bairi>
2016-08-31 21:16:55 +0530

    deleted some files

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import cookie from 'react-cookie';
-import { protectedTest } from '../../actions/auth';
 
 class Dashboard extends Component {
 
   constructor(props) {
     super(props);
-    this.props.protectedTest();
-  }
-
-  isRole(roleToCheck, toRender) {
-    let userRole = cookie.load('user').role;
-
-    if (userRole == roleToCheck) {
-      return toRender;
-    }
-
-    return false;
+    // this.props.protectedTest();
   }
 
   render() {
@@ -34,4 +22,4 @@ function mapStateToProps(state) {
   return { content: state.auth.content };
 }
 
-export default connect(mapStateToProps, { protectedTest })(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
